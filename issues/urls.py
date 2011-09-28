b0447be5044d4ff3faef36cd590b93eba77d6567
Stijn Debrouwere <stijn@stdout.be>
2011-09-28 14:50:20 +0200

    Fixed url routing mess-up.

--- issues/urls.py
+++ issues/urls.py
@@ -7,9 +7,9 @@ admin.autodiscover()
 urlpatterns = patterns('',
     surl('accounts/login/', 'django.contrib.auth.views.login', 
         {'template_name': 'login.html'}),
-    surl('$', 'issues.views.overview'),
     surl('latest/', 'issues.views.latest'),
     surl('<issue:s>/discover/', 'issues.views.discover'),
     surl('<issue:s>/discuss/', 'issues.views.discuss'),
     surl('<issue:s>/resolve/', 'issues.views.resolve'),
+    surl('^$', 'issues.views.overview'),
 )
