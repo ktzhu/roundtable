eecd54c85f9d4740bff84b214cf75fc8693a33b6
Stijn Debrouwere <stijn@stdout.be>
2011-09-28 12:55:26 +0200

    Miss moderator is now 37% more powerful.

--- issues/fixture_gen.py
+++ issues/fixture_gen.py
@@ -10,7 +10,7 @@ now = datetime.today()
 def test_users():
     users = issues.User.objects
     users.create(username='missmoderator', 
-        first_name='Miss', last_name='Moderator', is_superuser=True)
+        first_name='Miss', last_name='Moderator', is_superuser=True, is_staff=True)
     users.create(username='james.joyce', first_name='James', last_name='Joyce')
     users.create(username='john.dewey', first_name='John', last_name='Dewey')
     users.create(username='hannah.arendt', first_name='Hannah', last_name='Arendt')    
@@ -34,8 +34,8 @@ def test_issues():
         )
 
     _issues.create(
-        name = 'How can colleges be improved to combat the trend of credential inflation?',
-        description = 'Read the title, why don\'t you?',
+        name = "How can colleges be improved to combat the trend of credential inflation?",
+        description = "Read the title, why don't you?",
         discover = now - timedelta(hours=10),
         discuss = now - timedelta(hours=8),
         resolve = now - timedelta(hours=2),
@@ -74,9 +74,9 @@ def test_comments():
     issue1, issue2 = issues.Issue.objects.all()
     mod, user1, user2, user3, user4 = issues.User.objects.all()
         
-    _comments.create(content_object=issue1, user=user1, site_id=1, comment='Hullo')
-    _comments.create(content_object=solution1, user=user1, site_id=1, comment='You are totally right. More clowns, especially sad ones, are what is required.')
-    _comments.create(content_object=solution1, user=user2, site_id=1, comment='James, I totally get what you\'re saying, but I guess I\'m just not sure.')
+    _comments.create(content_object=issue1, user=user1, site_id=1, comment="Hullo")
+    _comments.create(content_object=solution1, user=user1, site_id=1, comment="You are totally right. More clowns, especially sad ones, are what is required.")
+    _comments.create(content_object=solution1, user=user2, site_id=1, comment="James, I totally get what you're saying, but I guess I'm just not sure.")
     
 
 @fixture_generator(requires=['issues.test_users', 'issues.test_issues', 'issues.test_solutions', 'issues.test_comments', 'issues.test_ratings', ])
